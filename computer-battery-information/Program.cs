83384469a0916d02abde5f96a4376177ddf8235e
Mustafa KOCA <Mustafa KOCA@DESKTOP-EI1459I>
2024-11-01 17:40:27 +0300

    Add project files.

---
+++ computer-battery-information/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+
+namespace computer_battery_information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Mustafa Koca | PC YÖNETİMİ";
+
+            // Batarya Bilgisi
+            RunPowerShellCommand("Get-WmiObject Win32_Battery | Select-Object Name, EstimatedChargeRemaining");
+
+            // Çalışan Servisler
+            RunPowerShellCommand("Get-WmiObject Win32_Service | Where-Object { $_.State -eq 'Running' -and $_.StartMode -eq 'Manual' } | Select-Object Name");
+
+            // Çalışan Processler
+            RunPowerShellCommand("Get-Process | Select-Object Name");
+
+            // İşlemci Bilgisi
+            RunPowerShellCommand("Get-WmiObject Win32_Processor | Select-Object Name");
+
+            Console.ReadLine();
+        }
+
+        static void RunPowerShellCommand(string command)
+        {
+            using (Process powerShell = new Process())
+            {
+                powerShell.StartInfo.FileName = "powershell.exe";
+                powerShell.StartInfo.Arguments = "-Command " + command;
+                powerShell.StartInfo.RedirectStandardOutput = true;
+                powerShell.StartInfo.UseShellExecute = false;
+                powerShell.StartInfo.CreateNoWindow = true;
+
+                powerShell.Start();
+
+                string output = powerShell.StandardOutput.ReadToEnd();
+                Console.WriteLine(output);
+
+                powerShell.WaitForExit();
+            }
+        }
+    }
+}
